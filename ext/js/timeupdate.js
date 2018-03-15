e83afaebae928302cb74bda94720ffa9da3b7df3
George <gliu10000@gmail.com>
2018-03-15 15:54:19 -0400

    Update timeupdate.js

--- ext/js/timeupdate.js
+++ ext/js/timeupdate.js
@@ -1,1 +1,47 @@
-
+function timeUpdate () {
+  var currTime = new Date();
+  var hrs = currTime.getHours();
+  var min = currTime.getMinutes();
+  var minsFromDayStart = hrs * 60 + min;
+  
+  var timeTable = [
+    {
+      "time":531,
+      "period":"Period 1"
+    },
+    {
+      "time":587,
+      "period":"Period 2"
+    },
+    {
+      "time":643,
+      "period":"Period 3"
+    },
+    {
+      "time":699,
+      "period":"Period 4"
+    },
+    {
+      "time":750,
+      "period":"1st Lunch"
+    },
+    {
+      "time":801,
+      "period":"Period 5A"
+    },
+    {
+      "time":852,
+      "period":"2nd Lunch"
+    },
+    {
+      "time":903,
+      "period":"Period 5B"
+    },
+    {
+      "time":954,
+      "period":"Period 7"
+    },
+  ]
+  
+  
+}
