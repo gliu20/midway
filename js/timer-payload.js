c5c34ed2c5da43c1cde8efbe756c64771d002a76
George <gliu10000@gmail.com>
2018-03-15 10:29:07 -0400

    Delete timer-payload.js

--- js/timer-payload.js
+++
@@ -1,3 +0,0 @@
-var div = document.createElement('div');
-div.innerHTML = '<header id=midway-timer class=midway-mdc-toolbar style=display:none;user-select:none;position:fixed;top:0;left:0;z-index:99999;><div class=midway-mdc-toolbar__row><section class="midway-mdc-toolbar__section"><span id=midway-timer-info class=midway-mdc-toolbar__title></span></section></div></header>';
-document.body.appendChild(div);
