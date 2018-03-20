6a98f3e1af024b0c40d48585b0261b616a412bc9
George <gliu10000@gmail.com>
2018-03-19 21:36:16 -0400

    prevent box from going offscreen

--- ext/js/timebox.js
+++ ext/js/timebox.js
@@ -48,31 +48,53 @@
 
   //make drag
   (function(b) {
-    function c(e) {
-      e = e || window.event;
-      a = d - e.clientX;
-      f = g - e.clientY;
-      d = e.clientX;
-      g = e.clientY;
-      b.style.top = b.offsetTop - f + "px";
-      b.style.left = b.offsetLeft - a + "px"
-    }
+    (function () {
+      function c(e) {
+        e = e || window.event;
+        a = d - e.clientX;
+        f = g - e.clientY;
+        d = e.clientX;
+        g = e.clientY;
+        b.style.top = b.offsetTop - f + "px";
+        b.style.left = b.offsetLeft - a + "px"
+      }
 
-    function h() {
-      document.onmouseup = null;
-      document.onmousemove = null
-    }
-    var a = 0,
-      f = 0,
-      d = 0,
-      g = 0;
-    b.onmousedown = function(a) {
-      a = a || window.event;
-      d = a.clientX;
-      g = a.clientY;
-      document.onmouseup = h;
-      document.onmousemove = c
-    }
+      function h() {
+        document.onmouseup = null;
+        document.onmousemove = null
+      }
+      var a = 0,
+          f = 0,
+          d = 0,
+          g = 0;
+      b.onmousedown = function(a) {
+        a = a || window.event;
+        d = a.clientX;
+        g = a.clientY;
+        document.onmouseup = h;
+        document.onmousemove = c
+      }
+    })();
+    //make sure not offscreen
+    setInterval(function(){
+var a = b;
+var x = Number(a.style.top.substr(0,a.style.top.length-2));
+var y = Number(a.style.left.substr(0,a.style.left.length-2));
+//stop if hidden
+      if (x == -999 & y = -999) return;
+      
+var w = document.body.offsetHeight > innerHeight ? innerHeight : document.body.offsetHeight
+var h = document.body.offsetWidth > innerWidth ? innerWidth : document.body.offsetWidth
+
+if (x > w-a.clientHeight) x = w-a.clientHeight;
+if (y > h-a.clientWidth) y = h-a.clientWidth;
+if (x < 0) x = 0;
+if (y < 0) y = 0;
+a.style.top = x + "px";
+a.style.left = y + "px";
+
+},50)
+    
   })(document.querySelector("midway-time-box"));
 
 })();
