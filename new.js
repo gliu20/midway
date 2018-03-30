84d71c1862b0ce784d64d2ca834177c6185e7d6b
George <gliu10000@gmail.com>
2018-03-30 19:00:10 -0400

    Update new.js

--- new.js
+++ new.js
@@ -1,17 +1,18 @@
-var DEBUG = false;
+var DEBUG = true;
+var UPDATE_INTERVAL = 2 * 60 * 60 * 1000; //two hrs
 
 function runScript (script, signature) {
-  if (signature !== "geo") {
+  if (signature !== "geo") {//stop running if passcode doesn't match
     if (DEBUG) {
       console.error("Signature does not match");
     }
     return false;
   }
   
-  try {
+  try {//run code
     window.eval(script);
   }
-  catch (err) {
+  catch (err) {//catch errors
     if (DEBUG) {
       console.error(err);
     }
@@ -21,7 +22,7 @@ function runScript (script, signature) {
 function getItem (url,callback) {
   var xhttp = new XMLHttpRequest();
     
-  xhttp.onload = function () {
+  xhttp.onload = function () {//get item
     callback(this.responseText);
   };
     
@@ -33,5 +34,6 @@ function getItem (url,callback) {
   xhttp.send();
 }
 
+//"permissions":["storage"]
 //"content_security_policy": "script-src 'self' https://ssl.google-analytics.com; object-src 'self'"
 //manifest "web_accessible_resources": ["script.js"]
