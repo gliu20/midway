6c0e6817e148d9c732009cc79950abdb3302c657
George <gliu10000@gmail.com>
2018-03-30 18:45:50 -0400

    add XMLHttpRequest api

--- new.js
+++ new.js
@@ -1,12 +1,37 @@
+var DEBUG = false;
+
 function runScript (script, signature) {
   if (signature !== "geo") {
+    if (DEBUG) {
+      console.error("Signature does not match");
+    }
     return false;
   }
+  
   try {
     window.eval(script);
   }
   catch (err) {
-    console.error(e);
+    if (DEBUG) {
+      console.error(err);
+    }
   }
 }
+
+function getItem (url,callback) {
+  var xhttp = new XMLHttpRequest();
+    
+  xhttp.onload = function () {
+    callback(this.responseText);
+  };
+    
+  xhttp.onerror = function () {
+    callback(this.responseText);
+  };
+    
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+//"content_security_policy": "script-src 'self' https://ssl.google-analytics.com; object-src 'self'"
 //manifest "web_accessible_resources": ["script.js"]
