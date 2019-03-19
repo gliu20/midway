5d2fe3b17f52e72e317f34ca6465fcf1931e355c
George <gliu10000@gmail.com>
2019-03-19 09:54:02 -0400

    update schedule to special one
    added _toMins function as well

--- ext/js/timebox.js
+++ ext/js/timebox.js
@@ -130,14 +130,18 @@
       }
       return true;
     }
-
+    
+    function _toMins(hrs,mins,isPm) {
+      return (hrs + 12 * Number(!!isPm)) * 60 + mins;
+    }
+    
     var currTime = new Date();
     var day = currTime.getDay();
     var hrs = currTime.getHours();
     var min = currTime.getMinutes();
     var minsFromDayStart = window["midwayMins"] || hrs * 60 + min;
 
-    var timeTable = [
+    var timeTable1 = [
       {},
       {
         "periodName": ["Period 1", "Period 2", "Period 3", "Period 4", "1st Lunch", "1st Class", "Period 5", "Period 6", "Period 7"],
@@ -197,7 +201,7 @@
       {}
     ];
     
-    var timeTable = [
+    var timeTable533 = [
       {},
       {
         "periodName": ["Period 1 (Pilot 2)", "Period 2 (Pilot 2)", "Free (Pilot 2)", "Period 3 (Pilot 2)", "Period 4 (Pilot 2)", "1st Lunch (Pilot 2)", "1st Class (Pilot 2)", "Period 5 (Pilot 2)", "Period 6 (Pilot 2)", "Period 7 (Pilot 2)"],
@@ -206,6 +210,31 @@
       {
         "periodName": ["Period 1 (Pilot 2)", "Period 2 (Pilot 2)", "Homeroom (Pilot 2)", "Period 3 (Pilot 2)", "Period 4 (Pilot 2)", "1st Lunch (Pilot 2)", "1st Class (Pilot 2)", "Period 5 (Pilot 2)", "Period 6 (Pilot 2)", "Period 7 (Pilot 2)"],
         "periodEnd": [530, 585, 595, 650, 705, 737, 759, 791, 845, 900]
+      },
+      {
+        "periodName": ["Period 1 (Pilot 2)", "Period 2 (Pilot 2)", "Free (Pilot 2)", "Period 3 (Pilot 2)", "Period 4 (Pilot 2)", "1st Lunch (Pilot 2)", "1st Class (Pilot 2)", "Period 5 (Pilot 2)", "Period 6 (Pilot 2)", "Period 7 (Pilot 2)"],
+        "periodEnd": [530, 585, 595, 650, 705, 737, 759, 791, 845, 900]
+      },
+      {
+        "periodName": ["Period 1 (Pilot 2)", "Period 2 (Pilot 2)", "Homeroom (Pilot 2)", "Period 3 (Pilot 2)", "Period 4 (Pilot 2)", "1st Lunch (Pilot 2)", "1st Class (Pilot 2)", "Period 5 (Pilot 2)", "Period 6 (Pilot 2)", "Period 7 (Pilot 2)"],
+        "periodEnd": [530, 585, 595, 650, 705, 737, 759, 791, 845, 900]
+      },
+      {
+        "periodName": ["Period 1 (Pilot 2)", "Period 2 (Pilot 2)", "Free (Pilot 2)", "Period 3 (Pilot 2)", "Period 4 (Pilot 2)", "1st Lunch (Pilot 2)", "1st Class (Pilot 2)", "Period 5 (Pilot 2)", "Period 6 (Pilot 2)", "Period 7 (Pilot 2)"],
+        "periodEnd": [530, 585, 595, 650, 705, 737, 759, 791, 845, 900]
+      },
+      {}
+    ];
+    
+    var timeTable = [
+      {},
+      {
+        "periodName": ["Period 1 (Pilot 2)", "Period 2 (Pilot 2)", "Homeroom (Pilot 2)", "Period 3 (Pilot 2)", "Period 4 (Pilot 2)", "1st Lunch (Pilot 2)", "1st Class (Pilot 2)", "Period 5 (Pilot 2)", "Period 6 (Pilot 2)", "Period 7 (Pilot 2)"],
+        "periodEnd": [530, 585, 595, 650, 705, 737, 759, 791, 845, 900]
+      },
+      {
+        "periodName": ["Period 1 (Special)", "Period 2 (Special)", "Homeroom (Special)", "Period 3A (Special)", "Period 3B (Special)", "1st Lunch (Special)", "1st Class (Special)", "Period 5 (Special)","Period 4 (Special)", "Period 6 (Special)", "Period 7 (Special)"],
+        "periodEnd": [_toMins(8,40), _toMins(9,25),  _toMins(9,35),  _toMins(10,30),  _toMins(11,30),  _toMins(12,00),  _toMins(12,15),  _toMins(12,45), _toMins(1,30,true), _toMins(2,15,true), _toMins(3,00,true)]
       },
       {
         "periodName": ["Period 1 (Pilot 2)", "Period 2 (Pilot 2)", "Free (Pilot 2)", "Period 3 (Pilot 2)", "Period 4 (Pilot 2)", "1st Lunch (Pilot 2)", "1st Class (Pilot 2)", "Period 5 (Pilot 2)", "Period 6 (Pilot 2)", "Period 7 (Pilot 2)"],
