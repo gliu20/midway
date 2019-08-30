5ba1d0e731d7bbe16df17dd1e06932a1f0f63b9c
George <gliu10000@gmail.com>
2019-08-30 00:32:12 -0400

    add re-factored midway apis
    for timebox, background, and content script timebox

--- midway-3.0.1/background/midway-timebox.js
+++ midway-3.0.1/background/midway-timebox.js
@@ -14,15 +14,22 @@ timebox.sendMessageToAll = function (mesgObj) {
 }
 
 timebox.updatePosition = function (x,y) {
-	timebox.sendMessageToAll({ type: "updatePosition",x,y });
+	timebox.sendMessageToAll({ type: "toContentScript-updatePosition",x,y });
 }
 
-timebox.showTimebox = function () {
-	timebox.sendMessageToAll({ type: "showTimebox" });
+timebox.show = function () {
+	timebox.sendMessageToAll({ type: "toContentScript-showTimebox" });
 }
 
-timebox.hideTimebox = function () {
-	timebox.sendMessageToAll({ type: "hideTimebox" });
+timebox.hide = function () {
+	timebox.sendMessageToAll({ type: "toContentScript-hideTimebox" });
+}
+
+timebox.display = function (line1,line2,line3) {
+	timebox.sendMessageToAll({
+		type: "toContentScript-displayCurrentPeriodInfo",
+		line1,line2,line3
+	});
 }
 
 // how timebox comm works
@@ -32,4 +39,47 @@ timebox.hideTimebox = function () {
 // 4. timebox gets moved telling bg its position and visibility
 // 5. bg broadcasts movement of timebox and visibility
 
-
+chrome.runtime.onMessage.addListener(
+	function (request, sender, sendResponse) {
+		if (request.type === "toBackground-storePosition") {
+			timebox.x = request.x;
+			timebox.y = request.y;
+			
+			timebox.updatePosition(request.x,request.y);
+		}
+		else if (request.type === "toBackground-returnPosition") {
+			timebox.updatePosition(timebox.x,timebox.y);
+			
+			if (timebox.isVisible) {
+				timebox.show();
+			}
+			else {
+				timebox.hide();
+			}
+		}
+		else if (request.type === "toBackground-returnCurrentPeriodInfo") {
+			// TODO return current period info
+		}
+		else if (request.type === "toBackground-hideTimebox") {
+			timebox.hide();
+			timebox.isVisible = false;
+		}
+		else if (request.type === "toBackground-showTimebox") {
+			timebox.show();
+			timebox.isVisible = true;
+		}
+		else if (request.type === "toBackground-toggleTimebox") {
+			timebox.isVisible = !timebox.isVisible;
+			
+			if (timebox.isVisible) {
+				timebox.show();
+			}
+			else {
+				timebox.hide();
+			}
+		}
+		else if (request.type === "toBackground-resetPosition") {
+			timebox.updatePosition(timebox.x = "0px",timebox.y = "0px");
+		}
+	}
+);
