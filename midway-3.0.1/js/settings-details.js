22abadedf5a6be71638fa296771d6a9ec9ffb274
George <gliu10000@gmail.com>
2020-01-04 10:41:04 -0500

    delete old

--- midway-3.0.1/js/settings-details.js
+++
@@ -1,4 +0,0 @@
-// add version info
-document.querySelectorAll(".version").forEach(function (item) {
-	item.innerText = chrome.runtime.getManifest().version;
-})
--- midway-3.0.1/js/welcome.js
+++
@@ -1,9 +0,0 @@
-// redirect to welcome page
-if (window.location.hash === "") {
-	var redirect = document.createElement("a");
-	
-	redirect.href = "#welcome";
-	
-	document.body.appendChild(redirect);
-	redirect.click();
-}
