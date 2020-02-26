82499eff9ce97dec7d67846bc72e03ff97a660ea
George <gliu10000@gmail.com>
2020-02-26 08:04:34 -0500

    Update firebase-config.js

--- background/firebase-config.js
+++ background/firebase-config.js
@@ -1,13 +1,15 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyBNCdvM5pzkiSuHKGOolx0HuFgM2VBWITU",
     authDomain: "midway-application.firebaseapp.com",
     databaseURL: "https://midway-application.firebaseio.com",
     projectId: "midway-application",
     storageBucket: "midway-application.appspot.com",
     messagingSenderId: "252261994154",
-    appId: "1:252261994154:web:b0efaf93bcebd689"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    appId: "1:252261994154:web:b0efaf93bcebd689",
+    measurementId: "G-E72NZ6MG69"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
