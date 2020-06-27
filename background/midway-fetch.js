3de66ff086b9432966d52b7c236f24705ffd88d8
gliu20 <gliu10000@gmail.com>
2020-06-27 18:11:50 -0400

    create new fetch for midway backend

---
+++ background/midway-fetch.js
@@ -0,0 +1,37 @@
+const fetchLookup = {
+    absentTeachers: ({schoolId}) => `schools/${schoolId}/absentTeachers`,
+    announcements: ({schoolId}) => `schools/${schoolId}/announcements`,
+    schoolSchedule: ({schoolId,dateString}) => `schools/${schoolId}/schoolSchedule/${dateString}`,
+    updateInterval: ({schoolId}) => `schools/${schoolId}/updateInterval`
+};
+
+midway.fetch = async function (type,identifiers,idToken,{urlParams,requestOptions}) {
+
+    // check cache
+    const cachedData = cache.get(type);
+
+    if (cachedData.success) {
+        // skip fetch if recent cached data is found
+        return cachedData.data;
+    }
+
+    const fetchPath = fetchLookup[type](identifiers);
+    const fullUrl = [
+        firebaseConfig.databaseURL,
+        `/${fetchPath}`,
+        `.json?auth=${idToken}`,
+        urlParams ? `&${urlParams}` : ""
+    ].join("");
+
+    const data = fetch(fullUrl, requestOptions)
+        .then(response => {
+            if (!response.ok) { throw new Error (response.statusText) }
+            return response;
+        });
+
+    // update cache
+    cache.set(type,data);
+
+    return data;
+
+}
