fbe22e82dcea0cbc2520f928ce5d4afacc70bd6f
gliu20 <gliu10000@gmail.com>
2020-06-27 18:32:21 -0400

    add school code fetch url

--- background/midway-fetch.js
+++ background/midway-fetch.js
@@ -2,7 +2,8 @@ const fetchLookup = {
     absentTeachers: ({schoolId}) => `schools/${schoolId}/absentTeachers`,
     announcements: ({schoolId}) => `schools/${schoolId}/announcements`,
     schoolSchedule: ({schoolId,dateString}) => `schools/${schoolId}/schoolSchedule/${dateString}`,
-    updateInterval: ({schoolId}) => `schools/${schoolId}/updateInterval`
+    updateInterval: ({schoolId}) => `schools/${schoolId}/updateInterval`,
+    schoolCode: ({userId}) => `users/${user.uid}/schoolCode`
 };
 
 midway.fetch = async function (type,identifiers,idToken,{urlParams,requestOptions}) {
