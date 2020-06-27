64d4c7350344b405882fe80873a604fd61248cc5
gliu20 <gliu10000@gmail.com>
2020-06-27 19:25:11 -0400

    refactor to cache and web fetch versions

--- background/midway-fetch.js
+++ background/midway-fetch.js
@@ -1,4 +1,6 @@
-const fetchLookup = {
+midway.fetch = {};
+
+midway.fetch.fetchLookup = {
     absentTeachers: ({schoolId}) => `schools/${schoolId}/absentTeachers`,
     announcements: ({schoolId}) => `schools/${schoolId}/announcements`,
     schoolSchedule: ({schoolId,dateString}) => `schools/${schoolId}/schoolSchedule/${dateString}`,
@@ -6,35 +8,39 @@ const fetchLookup = {
     $lastUpdated: ({schoolId}) => `schools/${schoolId}/lastUpdated`
 };
 
-midway.fetch = async function (type,identifiers,idToken,{urlParams,requestOptions}) {
+midway.fetch.fromWeb = async function (type,identifiers,idToken,{urlParams,requestOptions}) {
+
+    const fetchPath = fetchLookup[type](identifiers);
+    const urlParams = urlParams ? `&${urlParams}` : "";
+    const fullUrl = `${firebaseConfig.databaseURL}/${fetchPath}.json?auth=${idToken}${urlParams}`;
+
+    return fetch(fullUrl, requestOptions)
+        .then(response => {
+            if (!response.ok) { throw new Error (response.statusText) }
+            return response;
+        })
+        .then(response => response.json());
+    
+}
+
+midway.fetch.fromCache = async function (type,identifiers,idToken,{urlParams,requestOptions}) {
 
     if (type.charAt(0) === "$") {
-        return new Error ("Fetch should not be used to obtain special/non-cachable values");
+        return new Error ("cachedFetch should not be used to obtain special/non-cachable values");
     }
 
     // check cache
     const cachedData = cache.get(type);
 
     if (cachedData.success) {
+        // cache age is appropriate and available, so return cached data
         return cachedData.data;
     }
 
-    // cache missed
-    // now we need to figure out if we should update
-    const fetchPath = fetchLookup[type](identifiers);
-    const urlParams = urlParams ? `&${urlParams}` : "";
-    const fullUrl = `${firebaseConfig.databaseURL}/${fetchPath}.json?auth=${idToken}${urlParams}`;
+    // cache missed, so fetch the data
+    const data = midway.fetch.fromWeb(type,identifiers,idToken,{urlParams,requestOptions});
 
-    const data = fetch(fullUrl, requestOptions)
-        .then(response => {
-            if (!response.ok) { throw new Error (response.statusText) }
-            return response;
-        })
-        .then(response => response.json());
-
-    // update cache
-    cache.set(type,data);
-
-    return data;
+    // update cache and return data
+    return cache.set(type,data);
 
 }
