51694d792120518dda999e12f2267777d08f07bd
gliu20 <gliu10000@gmail.com>
2020-06-27 20:11:02 -0400

    add simple fetch (w/o assumptions)

--- background/midway-fetch.js
+++ background/midway-fetch.js
@@ -1,32 +1,35 @@
 midway.fetch = {};
 
 midway.fetch.fetchLookup = {
-    absentTeachers: ({schoolId}) => `schools/${schoolId}/absentTeachers`,
-    announcements: ({schoolId}) => `schools/${schoolId}/announcements`,
-    schoolSchedule: ({schoolId,dateString}) => `schools/${schoolId}/schoolSchedule/${dateString}`,
-    schoolCode: ({userId}) => `users/${userId}/schoolCode`,
-    $lastUpdated: ({schoolId}) => `schools/${schoolId}/lastUpdated`
+    absentTeachers: ({ schoolId }) => `schools/${schoolId}/absentTeachers`,
+    announcements: ({ schoolId }) => `schools/${schoolId}/announcements`,
+    schoolSchedule: ({ schoolId, dateString }) => `schools/${schoolId}/schoolSchedule/${dateString}`,
+    schoolCode: ({ userId }) => `users/${userId}/schoolCode`,
+    $lastUpdated: ({ schoolId }) => `schools/${schoolId}/lastUpdated`
 };
 
-midway.fetch.fromWeb = async function (type,identifiers,idToken,{urlParams,requestOptions}) {
+midway.fetch.fetch = async function (url, requestOptions) {
+    return fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) { throw new Error(response.statusText) }
+            return response;
+        })
+        .then(response => response.json());
+}
+
+midway.fetch.fromDatabase = async function (type, identifiers, idToken, { urlParams, requestOptions }) {
 
     const fetchPath = fetchLookup[type](identifiers);
     const urlParams = urlParams ? `&${urlParams}` : "";
     const fullUrl = `${firebaseConfig.databaseURL}/${fetchPath}.json?auth=${idToken}${urlParams}`;
 
-    return fetch(fullUrl, requestOptions)
-        .then(response => {
-            if (!response.ok) { throw new Error (response.statusText) }
-            return response;
-        })
-        .then(response => response.json());
-    
+    return midway.fetch.fetch(fullUrl, requestOptions);
 }
 
-midway.fetch.fromCache = async function (type,identifiers,idToken,{urlParams,requestOptions}) {
+midway.fetch.fromCache = async function (type, identifiers, idToken, { urlParams, requestOptions }) {
 
     if (type.charAt(0) === "$") {
-        return new Error ("cachedFetch should not be used to obtain special/non-cachable values");
+        return new Error("cachedFetch should not be used to obtain special/non-cachable values");
     }
 
     // check cache
@@ -38,9 +41,9 @@ midway.fetch.fromCache = async function (type,identifiers,idToken,{urlParams,req
     }
 
     // cache missed, so fetch the data
-    const data = midway.fetch.fromWeb(type,identifiers,idToken,{urlParams,requestOptions});
+    const data = midway.fetch.fromDatabase(type, identifiers, idToken, { urlParams, requestOptions });
 
     // update cache and return data
-    return cache.set(type,data);
+    return cache.set(type, data);
 
 }
