c57ceef650b5e7ca48834a5230420fc305c3bfd3
gliu20 <gliu10000@gmail.com>
2020-06-28 18:13:34 -0400

    add config to midway-app

--- background/midway-app.js
+++ background/midway-app.js
@@ -1,8 +1,12 @@
 const midway = {};
 
+midway.config = {
+    platform: "EXTENSION" // EXTENSION || WEB
+}
+
 midway.init = function () {
     // start up clock service
     // start up authentication process (check if sign in available)
-
-    
+    // start up storage process and syncing with different accounts
+    // check for welcome message or onboarding process
 }
