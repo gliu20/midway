9e9f0850d3d58b4f6d4af0ea223a15b42b42edc0
gliu20 <gliu10000@gmail.com>
2020-06-28 18:15:49 -0400

    move firebase initalization to midway app

--- background/midway-app.js
+++ background/midway-app.js
@@ -1,7 +1,18 @@
 const midway = {};
 
 midway.config = {
-    platform: "EXTENSION" // EXTENSION || WEB
+    platform: "EXTENSION", // EXTENSION || WEB
+    firebaseConfig: {
+        apiKey: "AIzaSyBNCdvM5pzkiSuHKGOolx0HuFgM2VBWITU",
+        authDomain: "midway-application.firebaseapp.com",
+        databaseURL: "https://midway-application.firebaseio.com",
+        projectId: "midway-application",
+        storageBucket: "midway-application.appspot.com",
+        messagingSenderId: "252261994154",
+        appId: "1:252261994154:web:b0efaf93bcebd689",
+        measurementId: "G-E72NZ6MG69"
+    },
+    enableAnalytics: false
 }
 
 midway.init = function () {
@@ -9,4 +20,11 @@ midway.init = function () {
     // start up authentication process (check if sign in available)
     // start up storage process and syncing with different accounts
     // check for welcome message or onboarding process
+
+    // init firebase
+    firebase.initializeApp(firebaseConfig);
+
+    if (enableAnalytics) {
+        firebase.analytics();
+    }
 }
