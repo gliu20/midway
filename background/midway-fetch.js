a527e241586b9d01c720ceaa3104d73c05d8a695
gliu20 <gliu10000@gmail.com>
2020-06-28 14:48:44 -0400

    add cache reuse for when new data isn't available

--- background/midway-fetch.js
+++ background/midway-fetch.js
@@ -7,6 +7,7 @@ midway.fetch.fetchLookup = {
     schoolCode: ({ userId }) => `users/${userId}/schoolCode`,
     emailLookup: ({ schoolDomain }) => `emailLookup/${schoolDomain}`,
     $lastUpdated: ({ schoolId }) => `schools/${schoolId}/lastUpdated`,
+    $cacheAge: ({ schoolId }) => `schools/${schoolId}/cacheAge`,
     $clock: () => `util/time`
 };
 
@@ -31,6 +32,7 @@ midway.fetch.fromDatabase = async function (type, identifiers, idToken, { urlPar
 
 midway.fetch.fromCache = async function (type, identifiers, idToken, { urlParams, requestOptions }) {
 
+    // TODO add schoolId getter
     const cacheId = midway.fetch.generateCacheId(type,identifiers);
 
     if (type.charAt(0) === "$") {
@@ -45,12 +47,22 @@ midway.fetch.fromCache = async function (type, identifiers, idToken, { urlParams
         return cachedData.data;
     }
 
-    // cache missed, so fetch the data
+    // cache missed, but we don't know if we should still keep the old data
+    // to determine this, check if the data has been updated
+    const lastUpdated = midway.fetch.fromDatabase("$lastUpdated",schoolId,idToken);
+
+    if (lastUpdated < midway.clock.now() && cachedData.oldDataAvailable) {
+        // server data hasn't been updated yet and cache still has old data
+        // that we can reuse, so we give it back to client
+        return cachedData.data;
+    }
+
+    // new data is required since cache doesn't have it or the data 
+    // isn't recent enough
     const data = midway.fetch.fromDatabase(type, identifiers, idToken, { urlParams, requestOptions });
 
     // update cache and return data
     return cache.set(cacheId, data);
-
 }
 
 midway.fetch.generateCacheId = (type,identifiers) => {
